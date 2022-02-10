3e2e09c52608d739784c6b2c234cfcc2785fbb0b
Angad Kambli <angadkambli@gmail.com>
2022-02-10 22:07:42 +0530

    final render

--- src/main.rs
+++ src/main.rs
@@ -40,57 +40,22 @@ fn main() {
     let now = Instant::now();
 
     // Image
-    let aspect_ratio: f64 = 16.0 / 9.0;
-    let image_width: u32 = 400;
+    let aspect_ratio: f64 = 3.0 / 2.0;
+    let image_width: u32 = 600;
     let image_height: u32 = u32(image_width as f64 / aspect_ratio).unwrap();
-    let samples_per_pixel = 100;
+    let samples_per_pixel = 250;
     let max_depth: i32 = 50;
 
     // World
-    let mut world = hittable::HittableList::new();
-    let material_ground = Arc::new(Mutex::new(material::Material::Lambertian(
-        material::Lambertian::new(vector3::Color::new(0.8, 0.8, 0.0)),
-    )));
-    /*let material_center = Arc::new(Mutex::new(material::Material::Lambertian(
-        material::Lambertian::new(vector3::Color::new(0.7, 0.3, 0.3)),
-    )));*/
-    let material_center = Arc::new(Mutex::new(material::Material::Dielectric(
-        material::Dielectric::new(1.5),
-    )));
-    let material_left = Arc::new(Mutex::new(material::Material::Metal(material::Metal::new(
-        vector3::Color::new(0.8, 0.8, 0.8),
-    ))));
-    let material_right = Arc::new(Mutex::new(material::Material::Metal(material::Metal::new(
-        vector3::Color::new(0.8, 0.6, 0.2),
-    ))));
-    world = world.add(hittable::HittableObj::Sphere(sphere::Sphere::new(
-        vector3::Point::new(0.0, -100.5, -1.0),
-        100.0,
-        material_ground.clone(),
-    )));
-    world = world.add(hittable::HittableObj::Sphere(sphere::Sphere::new(
-        vector3::Point::new(0.0, 0.0, -1.0),
-        0.5,
-        material_center.clone(),
-    )));
-    world = world.add(hittable::HittableObj::Sphere(sphere::Sphere::new(
-        vector3::Point::new(-1.0, 0.0, -1.0),
-        0.5,
-        material_left.clone(),
-    )));
-    world = world.add(hittable::HittableObj::Sphere(sphere::Sphere::new(
-        vector3::Point::new(1.0, 0.0, -1.0),
-        0.5,
-        material_right.clone(),
-    )));
+    let world = random_scene();
 
     // Camera
 
-    let lookfrom = vector3::Point::new(3.0, 3.0, 2.0);
-    let lookat = vector3::Point::new(0.0, 0.0, -1.0);
+    let lookfrom = vector3::Point::new(13.0, 2.0, 3.0);
+    let lookat = vector3::Point::new(0.0, 0.0, 0.0);
     let vup = vector3::Vec3::new(0.0, 1.0, 0.0);
-    let dist_to_focus = (lookfrom - lookat).length();
-    let aperture = 2.0;
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
 
     let cam = camera::Camera::new(
         lookfrom,
@@ -141,49 +106,94 @@ fn main() {
     println!("Image rendered in {:.2?}", elapsed);
 }
 
-/*
 pub fn random_scene() -> hittable::HittableList {
-    let world;
+    let mut world = hittable::HittableList::new();
 
-    let ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+    let ground_material = Arc::new(Mutex::new(material::Material::Lambertian(
+        material::Lambertian::new(vector3::Color::new(0.5, 0.5, 0.5)),
+    )));
+    world = world.add(hittable::HittableObj::Sphere(sphere::Sphere::new(
+        vector3::Point::new(0.0, -1000.0, -0.0),
+        1000.0,
+        ground_material.clone(),
+    )));
 
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
-            auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+    let mut rng = rand::thread_rng();
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rng.gen_range(0.0..1.0);
+            let center = vector3::Point::new(
+                a as f64 + 0.9 * rng.gen_range(0.0..1.0),
+                0.2,
+                b as f64 + 0.9 * rng.gen_range(0.0..1.0),
+            );
 
-            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-                shared_ptr<material> sphere_material;
-
-                if (choose_mat < 0.8) {
+            if (center - vector3::Point::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 {
                     // diffuse
-                    auto albedo = color::random() * color::random();
-                    sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                } else if (choose_mat < 0.95) {
+                    let albedo =
+                        vector3::Color::random(0.0, 1.0) * vector3::Color::random(0.0, 1.0);
+                    let sphere_material = Arc::new(Mutex::new(material::Material::Lambertian(
+                        material::Lambertian::new(albedo),
+                    )));
+                    world = world.add(hittable::HittableObj::Sphere(sphere::Sphere::new(
+                        center,
+                        0.2,
+                        sphere_material,
+                    )));
+                } else if choose_mat < 0.95 {
                     // metal
-                    auto albedo = color::random(0.5, 1);
-                    auto fuzz = random_double(0, 0.5);
-                    sphere_material = make_shared<metal>(albedo, fuzz);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    let albedo = vector3::Color::random(0.5, 1.0);
+                    let fuzz = rng.gen_range(0.0..0.5);
+                    let sphere_material = Arc::new(Mutex::new(material::Material::Metal(
+                        material::Metal::new(albedo),
+                    )));
+                    world = world.add(hittable::HittableObj::Sphere(sphere::Sphere::new(
+                        center,
+                        0.2,
+                        sphere_material,
+                    )));
                 } else {
                     // glass
-                    sphere_material = make_shared<dielectric>(1.5);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    let sphere_material = Arc::new(Mutex::new(material::Material::Dielectric(
+                        material::Dielectric::new(1.5),
+                    )));
+                    world = world.add(hittable::HittableObj::Sphere(sphere::Sphere::new(
+                        center,
+                        0.2,
+                        sphere_material,
+                    )));
                 }
             }
         }
     }
 
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+    let material1 = Arc::new(Mutex::new(material::Material::Dielectric(
+        material::Dielectric::new(1.5),
+    )));
+    world = world.add(hittable::HittableObj::Sphere(sphere::Sphere::new(
+        vector3::Point::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
+    )));
 
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+    let material2 = Arc::new(Mutex::new(material::Material::Lambertian(
+        material::Lambertian::new(vector3::Color::new(0.4, 0.2, 0.1)),
+    )));
+    world = world.add(hittable::HittableObj::Sphere(sphere::Sphere::new(
+        vector3::Point::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
+    )));
 
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+    let material3 = Arc::new(Mutex::new(material::Material::Metal(material::Metal::new(
+        vector3::Color::new(0.4, 0.2, 0.1),
+    ))));
+    world = world.add(hittable::HittableObj::Sphere(sphere::Sphere::new(
+        vector3::Point::new(4.0, 1.0, 0.0),
+        1.0,
+        material3,
+    )));
 
     return world;
-}*/
+}
